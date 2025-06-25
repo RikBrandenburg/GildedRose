b176ae73c47f6b445dc46f5db8636c83fc8f737d
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2025-06-25 14:38:13 +0200

    Feat(Add an Item Wrapper class)

---
+++ php/src/ItemWrapper.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose;
+
+abstract class ItemWrapper
+{
+    public function __construct(
+        protected Item $item
+    ) {}
+
+    abstract public function update(): void;
+
+    /**
+     * Increase the quality of the item by a specified amount, up to a maximum of 50.
+     * 
+     * @param int $amount
+     * @return void
+     */
+    protected function increaseQuality(int $amount = 1): void
+    {
+        $this->item->quality = min(50, $this->item->quality + $amount);
+    }
+
+    /**
+     * Decrease the quality of the item by a specified amount, down to a minimum of 0.
+     * 
+     * @param int $amount
+     * @return void
+     */
+    protected function decreaseQuality(int $amount = 1): void
+    {
+        $this->item->quality = max(0, $this->item->quality - $amount);
+    }
+
+    /**
+     * Decrease the sellIn value of the item by 1.
+     * 
+     * @return void
+     */
+    protected function decreaseSellIn(): void
+    {
+        $this->item->sellIn -= 1;
+    }
+}
